0821cabbf3390e8b0bc79ef098668b86f545324d
Simon Prickett <simon@crudworks.org>
2012-10-21 14:38:20 -0400

    Tidy up
    Tidied up and standardized use of quotes and format function for string
    processing.

--- bart.py
+++ bart.py
@@ -7,32 +7,30 @@ file.close()
 
 dom = parseString(data)
 stations = dom.getElementsByTagName('stations')[0]
-stationData = {}
 
 for station in stations.getElementsByTagName('station'):
      stationAbbr = station.getElementsByTagName('abbr')[0].firstChild.nodeValue
-     #print 'http://api.bart.gov/api/etd.aspx?cmd=etd&orig=' + stationAbbr + '&key=MW9S-E7SL-26DU-VV8V'
 
-     file = urllib2.urlopen('http://api.bart.gov/api/etd.aspx?cmd=etd&orig=' + stationAbbr + '&key=MW9S-E7SL-26DU-VV8V')
+     file = urllib2.urlopen('http://api.bart.gov/api/etd.aspx?cmd=etd&orig={stationAbbr}&key=MW9S-E7SL-26DU-VV8V'.format(stationAbbr = stationAbbr))
      data = file.read()
      file.close()
 
      dom = parseString(data)
 
      stationName = dom.getElementsByTagName('name')[0].firstChild.nodeValue
-     print "********** DEPARTURES FROM {stationName} **********".format(stationName = stationName)
+     print '********** DEPARTURES FROM {stationName} **********'.format(stationName = stationName)
      
      stationEtds = dom.getElementsByTagName('etd')
 
      for etd in stationEtds:
-         print "Destination: {dest}".format(dest = etd.getElementsByTagName('destination')[0].firstChild.nodeValue) 
+         print 'Destination: {dest}'.format(dest = etd.getElementsByTagName('destination')[0].firstChild.nodeValue) 
 
          for estimate in etd.getElementsByTagName('estimate'):
              minutesToDeparture = estimate.getElementsByTagName('minutes')[0].firstChild.nodeValue
-             if (minutesToDeparture == "Leaving"):
-                  minutesToDeparture = "0"
+             if (minutesToDeparture == 'Leaving'):
+                  minutesToDeparture = '0'
              platform = estimate.getElementsByTagName('platform')[0].firstChild.nodeValue
              numCars = estimate.getElementsByTagName('length')[0].firstChild.nodeValue
              lineColor = estimate.getElementsByTagName('color')[0].firstChild.nodeValue
-             print "{numCars} car {lineColor} line train leaves in {minutesToDeparture} minutes from platform {platform}.".format(numCars = numCars, lineColor = lineColor, minutesToDeparture = minutesToDeparture, platform = platform)
+             print '{numCars} car {lineColor} line train leaves in {minutesToDeparture} minutes from platform {platform}.'.format(numCars = numCars, lineColor = lineColor, minutesToDeparture = minutesToDeparture, platform = platform)
 
