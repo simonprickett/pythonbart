a53f30f065d5020552af0e76a23e9b6cad98c43a
Simon Prickett <simon@crudworks.org>
2012-10-21 22:27:52 -0400

    Added functions
    Modularized the code a bit into 2 functions, one dealing with reading
    the data from BART the other with walking the in memory data model and
    printing it out.

--- bart.py
+++ bart.py
@@ -1,67 +1,71 @@
 import urllib2
 from xml.dom.minidom import parseString
 
-file = urllib2.urlopen('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V')
-data = file.read()
-file.close()
-
-dom = parseString(data)
-stations = dom.getElementsByTagName('stations')[0]
-stationData = {}
-
-print 'Begin reading data from SF BART API, please wait!'
-
-for station in stations.getElementsByTagName('station'):
-     stationAbbr = station.getElementsByTagName('abbr')[0].firstChild.nodeValue
-
-     file = urllib2.urlopen('http://api.bart.gov/api/etd.aspx?cmd=etd&orig={stationAbbr}&key=MW9S-E7SL-26DU-VV8V'.format(stationAbbr = stationAbbr))
+def readData():
+     file = urllib2.urlopen('http://api.bart.gov/api/stn.aspx?cmd=stns&key=MW9S-E7SL-26DU-VV8V')
      data = file.read()
      file.close()
 
      dom = parseString(data)
+     stations = dom.getElementsByTagName('stations')[0]
+     stationData = {}
 
-     stationName = dom.getElementsByTagName('name')[0].firstChild.nodeValue
+     print 'Begin reading data from SF BART API, please wait!'
+
+     for station in stations.getElementsByTagName('station'):
+          stationAbbr = station.getElementsByTagName('abbr')[0].firstChild.nodeValue
+
+          file = urllib2.urlopen('http://api.bart.gov/api/etd.aspx?cmd=etd&orig={stationAbbr}&key=MW9S-E7SL-26DU-VV8V'.format(stationAbbr = stationAbbr))
+          data = file.read()
+          file.close()
+
+          dom = parseString(data)
+
+          stationName = dom.getElementsByTagName('name')[0].firstChild.nodeValue
+          
+          stationEtds = dom.getElementsByTagName('etd')
+          stationDestinations = {}
+
+          for etd in stationEtds:
+              destination = etd.getElementsByTagName('destination')[0].firstChild.nodeValue
+
+              stationDestinationDepartures = []
+              
+              for estimate in etd.getElementsByTagName('estimate'):
+                  stationDestinationDeparture = {}
+                  
+                  minutesToDeparture = estimate.getElementsByTagName('minutes')[0].firstChild.nodeValue
+                  if (minutesToDeparture == 'Leaving'):
+                       minutesToDeparture = '0'
+                  platform = estimate.getElementsByTagName('platform')[0].firstChild.nodeValue
+                  numCars = estimate.getElementsByTagName('length')[0].firstChild.nodeValue
+                  lineColor = estimate.getElementsByTagName('color')[0].firstChild.nodeValue
+
+                  stationDestinationDeparture['platform'] = platform
+                  stationDestinationDeparture['numCars'] = numCars
+                  stationDestinationDeparture['lineColor'] = lineColor
+                  stationDestinationDeparture['minutesToDeparture'] = minutesToDeparture
+
+                  stationDestinationDepartures.append(stationDestinationDeparture)
+                  
+              stationDestinations[destination] = stationDestinationDepartures
+              
+          stationData[stationName] = stationDestinations
+
+     print 'Done reading data from SF BART API'
+     print ''
+     return stationData
+
+def printData(stationData):
+     for stationName in stationData.keys():
+          stationDestinations = stationData[stationName]
+          print '********** DEPARTURES FROM {stationName} **********'.format(stationName= stationName)
+          
+          for stationDestination in stationDestinations.keys():
+               print stationDestination
+
+               for departure in stationDestinations[stationDestination]:
+                    print '- {numCars} car {lineColor} line train leaves in {minutesToDeparture} minutes from platform {platform}.'.format(numCars = departure['numCars'], lineColor = departure['lineColor'], minutesToDeparture = departure['minutesToDeparture'], platform = departure['platform'])
+
      
-     stationEtds = dom.getElementsByTagName('etd')
-     stationDestinations = {}
-
-     for etd in stationEtds:
-         destination = etd.getElementsByTagName('destination')[0].firstChild.nodeValue
-
-         stationDestinationDepartures = []
-         
-         for estimate in etd.getElementsByTagName('estimate'):
-             stationDestinationDeparture = {}
-             
-             minutesToDeparture = estimate.getElementsByTagName('minutes')[0].firstChild.nodeValue
-             if (minutesToDeparture == 'Leaving'):
-                  minutesToDeparture = '0'
-             platform = estimate.getElementsByTagName('platform')[0].firstChild.nodeValue
-             numCars = estimate.getElementsByTagName('length')[0].firstChild.nodeValue
-             lineColor = estimate.getElementsByTagName('color')[0].firstChild.nodeValue
-
-             stationDestinationDeparture['platform'] = platform
-             stationDestinationDeparture['numCars'] = numCars
-             stationDestinationDeparture['lineColor'] = lineColor
-             stationDestinationDeparture['minutesToDeparture'] = minutesToDeparture
-
-             stationDestinationDepartures.append(stationDestinationDeparture)
-             
-         stationDestinations[destination] = stationDestinationDepartures
-         
-     stationData[stationName] = stationDestinations
-
-print 'Done reading data from SF BART API'
-print ''
-
-for stationName in stationData.keys():
-     stationDestinations = stationData[stationName]
-     print '********** DEPARTURES FROM {stationName} **********'.format(stationName= stationName)
-     
-     for stationDestination in stationDestinations.keys():
-          print stationDestination
-
-          for departure in stationDestinations[stationDestination]:
-               print '- {numCars} car {lineColor} line train leaves in {minutesToDeparture} minutes from platform {platform}.'.format(numCars = departure['numCars'], lineColor = departure['lineColor'], minutesToDeparture = departure['minutesToDeparture'], platform = departure['platform'])
-
-     
+printData(readData())
